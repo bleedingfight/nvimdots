74c554c3d8b8857169b320a66da2fa13d5dd8e46
liushuai <bleedingfight@126.com>
2023-03-18 14:41:41 +0800

    change completion

--- lua/modules/plugins/completion.lua
+++ lua/modules/plugins/completion.lua
@@ -43,14 +43,14 @@ completion["hrsh7th/nvim-cmp"] = {
 		{ "hrsh7th/cmp-buffer" },
 		{ "kdheepak/cmp-latex-symbols" },
 		{ "ray-x/cmp-treesitter" },
-		{ "tzachar/cmp-tabnine", build = "./install.sh", config = require("completion.tabnine") },
+		-- { "tzachar/cmp-tabnine", build = "./install.sh", config = require("completion.tabnine") },
 		{
-		 	"jcdickinson/codeium.nvim",
-		 	dependencies = {
-		 		"nvim-lua/plenary.nvim",
-		 		"MunifTanjim/nui.nvim",
-		 	},
-		 	config = require("completion.codeium"),
+			"jcdickinson/codeium.nvim",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				"MunifTanjim/nui.nvim",
+			},
+			config = require("completion.codeium"),
 		},
 	},
 }
