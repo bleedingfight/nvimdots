f63c1f1bee2612eb1f79a9d7fef08e1f4664d6ba
liushuai <bleedingfight@126.com>
2024-04-07 22:02:23 +0800

    删除无用依赖

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -8,26 +8,53 @@ settings["colorscheme"] = "catppuccin"
 
 settings["use_copilot"] = false
 
-settings["lsp_deps"] = {
-	"bashls",
-	"clangd",
-	"html",
-	"jsonls",
-	"lua_ls",
-	"ruff_lsp",
-	"pylsp",
-	"ruff_lsp",
-}
+settings["lsp_deps"] = function(defaults)
+	return {
+		defaults[0],
+		defaults[1],
+		defaults[2],
+		defaults[3],
+		defaults[4],
+		"ruff_lsp",
+		"rust-analyzer",
+	}
+end
 
-settings["dap_deps"] = {
-	"cpptools", -- C-Family
-	"python", -- Python (debugpy)
-}
+settings["dap_deps"] = function(defaults)
+	return {
+		defaults[0], -- C-Family
+		defaults[2], -- Python (debugpy)
+	}
+end
 
-settings["null_ls_deps"] = {
-	"clang_format",
-	"prettier",
-	"shfmt",
-	"stylua",
-}
+settings["null_ls_deps"] = function(defaults)
+	return {
+		defaults[0],
+		defaults[3],
+		defaults[4],
+		defaults[5],
+	}
+end
+
+settings["treesitter_deps"] = function(defaults)
+	return {
+		defaults[0],
+		defaults[1],
+		defaults[2],
+		defaults[3],
+		defaults[6],
+		defaults[7],
+		defaults[8],
+		defaults[9],
+		defaults[10],
+		defaults[11],
+		defaults[12],
+		defaults[13],
+		defaults[14],
+		defaults[15],
+		defaults[16],
+		defaults[17],
+		defaults[19],
+	}
+end
 return settings
