ffba08579d9fa18f0d6aaa579d869dd4ec9ffc85
liushuai <bleedingfight@126.com>
2024-06-22 09:42:02 +0800

    自定义treesitter

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,7 +1,6 @@
 -- Please check `lua/core/settings.lua` to view the full list of configurable settings
 local settings = {}
 
--- Examples
 settings["use_ssh"] = true
 
 settings["colorscheme"] = "catppuccin"
