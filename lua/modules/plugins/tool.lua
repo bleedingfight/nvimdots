bbc2e09d1f62d4b232514c641413374199fdf8ad
liushuai <bleedingfight@126.com>
2024-10-19 22:01:25 +0800

    更改which-key配置

--- lua/modules/plugins/tool.lua
+++ lua/modules/plugins/tool.lua
@@ -53,9 +53,21 @@ tool["folke/trouble.nvim"] = {
 	config = require("tool.trouble"),
 }
 tool["folke/which-key.nvim"] = {
-	lazy = true,
-	event = { "CursorHold", "CursorHoldI" },
-	config = require("tool.which-key"),
+	event = "VeryLazy",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
 tool["gelguy/wilder.nvim"] = {
 	lazy = true,
