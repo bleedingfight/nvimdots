8c7dc66cbc7e3a3be6a2c6e86d830eba79e020d7
liushuai <liushuai@shuaideMacBook-Pro.local>
2026-04-01 17:59:47 +0800

    更新smarkyark

--- lua/modules/configs/tool/smartyank.lua
+++ lua/modules/configs/tool/smartyank.lua
@@ -15,8 +15,7 @@ return function()
 		},
 		osc52 = {
 			enabled = true,
-			escseq = "tmux", -- use tmux escape sequence, only enable if you're using remote tmux and have issues (see #4)
-			ssh_only = true, -- false to OSC52 yank also in local sessions
+			ssh_only = true, -- false to OSC52 yank also in local sessions 需要设置SSH_CONNECTION
 			silent = false, -- true to disable the "n chars copied" echo
 			echo_hl = "Directory", -- highlight group of the OSC52 echo message
 		},
